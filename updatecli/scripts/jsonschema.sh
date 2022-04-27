3a7f922956582b5bcd5323ad2628db48ee1927b2
Olblak <me@olblak.com>
2022-04-27 21:01:43 +0200

    Remove useless comment
    Signed-off-by: Olblak <me@olblak.com>

--- updatecli/scripts/jsonschema.sh
+++ updatecli/scripts/jsonschema.sh
@@ -5,11 +5,9 @@ set -eux
 WORKDIR="."
 
 OLD_CHECKSUM=$(wc -c < $WORKDIR/content/schema/latest/config.json)
-#OLD_CHECKSUM=$(cat $WORKDIR/content/schema/latest/config.json | sha256sum)
 
 updatecli jsonschema --baseid https://www.updatecli.io/latest/schema --directory $WORKDIR/content/schema/latest
 
-#CHECKSUM=$(cat $WORKDIR/content/schema/latest/config.json | sha256sum)
 CHECKSUM=$(wc -c < $WORKDIR/content/schema/latest/config.json)
 
 if [ "$OLD_CHECKSUM" != "$CHECKSUM" ]; then
