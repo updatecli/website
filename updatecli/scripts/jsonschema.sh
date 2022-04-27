824aded22229467b20c6db16192e7f208b61896c
Olivier Vernin <olivier@vernin.me>
2022-04-27 21:04:18 +0200

    Update jsonschema script to use file size (#332)
    * Update jsonschema script to use file size
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Remove useless comment
    
    Signed-off-by: Olblak <me@olblak.com>

--- updatecli/scripts/jsonschema.sh
+++ updatecli/scripts/jsonschema.sh
@@ -4,12 +4,11 @@ set -eux
 
 WORKDIR="."
 
-OLD_CHECKSUM=$(cat $WORKDIR/content/schema/latest/config.json | md5sum)
+OLD_CHECKSUM=$(wc -c < $WORKDIR/content/schema/latest/config.json)
 
 updatecli jsonschema --baseid https://www.updatecli.io/latest/schema --directory $WORKDIR/content/schema/latest
 
-CHECKSUM=$(cat $WORKDIR/content/schema/latest/config.json | md5sum)
-
+CHECKSUM=$(wc -c < $WORKDIR/content/schema/latest/config.json)
 
 if [ "$OLD_CHECKSUM" != "$CHECKSUM" ]; then
     echo "Jsonschema change detected"
