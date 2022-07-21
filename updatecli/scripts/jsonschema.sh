8d8938486bd707bb63877b6b66c32876938ccfa9
Olblak <me@olblak.com>
2022-07-21 19:48:35 +0200

    Fix path
    Signed-off-by: Olblak <me@olblak.com>

--- updatecli/scripts/jsonschema.sh
+++ updatecli/scripts/jsonschema.sh
@@ -4,11 +4,11 @@ set -eux
 
 WORKDIR="."
 
-OLD_CHECKSUM=$(wc -c < $WORKDIR/content/schema/latest/config.json)
+OLD_CHECKSUM=$(wc -c < $WORKDIR/content/en/schema/latest/config.json)
 
 updatecli jsonschema --baseid https://www.updatecli.io/latest/schema --directory $WORKDIR/content/en/schema/latest
 
-CHECKSUM=$(wc -c < $WORKDIR/content/schema/latest/config.json)
+CHECKSUM=$(wc -c < $WORKDIR/content/en/schema/latest/config.json)
 
 if [ "$OLD_CHECKSUM" != "$CHECKSUM" ]; then
     echo "Jsonschema change detected"
